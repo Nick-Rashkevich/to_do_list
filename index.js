b43fb89432b2ad5431e31eb6884663b5f96abefc
Nick_R <86295744+Nick-Rashkevich@users.noreply.github.com>
2021-08-09 10:18:57 +0300

    add index.html and index.js

---
+++ index.js
@@ -0,0 +1,56 @@
+//ROOT
+const root = document.querySelector('#root')
+
+//DATA
+const todos = [
+    {
+        title: 'Загаловок дела N1',
+        text: 'Подробное описание задачи, которое может состоять из неограниченного количества слов.',
+        time: '14:00',
+    },
+    {
+        title: 'Загаловок дела N2',
+        text: 'Подробное описание задачи, которое может состоять из неограниченного количества слов.',
+        time: '15:00',
+    },
+]
+
+//APP
+render(todos)
+createInput()
+
+function createCard(content) {
+    const card = document.createElement('div')
+    const cardTitle = document.createElement('h2')
+    const cardText = document.createElement('p')
+    const cardTime = document.createElement('p')
+
+    card.className = 'flex flex-col w-80 h-48 bg-indigo-300 m-auto mt-4 p-2 shadow-lg rounded-lg text-justify'
+    cardTitle.className = 'text-2xl'
+    cardText.className = 'pt-2 h-full overflow-hidden'
+    cardTime.className = 'italic'
+
+    cardTitle.textContent = content.title
+    cardText.textContent = content.text
+    cardTime.textContent = content.time
+
+    root.className = 'flex flex-col'
+    card.append(cardTitle, cardText, cardTime)
+    root.append(card)
+}
+
+function render(model) {
+    model.forEach(someObj => {
+        createCard(someObj)
+    });
+}
+
+function createInput() {
+    const inputWindow = document.createElement('div')
+    const inputWindowElem = document.createElement('input')
+
+    inputWindowElem.className = 'rounded-lg border-2 bg-indigo-300'
+
+    inputWindow.append(inputWindowElem)
+    root.append(inputWindow)
+}
